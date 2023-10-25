b0cdd92330fc1fbfc840b02f231a24c5dad6767f
HoaDB <117150692+Hoa0304@users.noreply.github.com>
2023-10-25 10:59:08 +0700

    Create CalculatorInterface.java

---
+++ CalculatorInterface.java
@@ -0,0 +1,9 @@
+public interface CalculatorInterface {
+	public abstract double add(double a, double b);
+	
+	public double sub(double a, double b);
+	
+	public double muliple(double a, double b);
+	
+	public double div(double a, double b);
+}
